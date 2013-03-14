34eb9912b6b98e861e2b08629b36d3a6fbdb0cfd
Scott Tsai <scottt.tw@gmail.com>
2013-03-15 07:18:58 +0800

    9.m: fix (b), (c) mix up

--- hw1/9.m
+++ hw1/9.m
@@ -4,7 +4,8 @@ printf('(a) p.m.f.: P(X=x) = (13 - 2*x)/36\n');
 X = 1:6;
 P = arrayfun(Px, X);
 %!test assert(sum(p) == 1)
-bar(X, P, 1);  % (b) draw probability histogram
+printf('(b) See plot\n');
+bar(X, P, 1);
 title('Prob. 9: Dice throwing probability histogram, min of two');
 xlabel('dice face, min of two, six sided');
 ux = X * P';
@@ -12,7 +13,7 @@ sx2 = arrayfun(@(x) x^2, X) * P';
 varx = sx2 - ux^2;
 varx_ = arrayfun(@(x) (x - ux)^2, X) * P'; % alternative way to calculate varx
 %!test assert(varx == varx_)
-printf('(b) ux: %f (i.e. 301/36)\n(c) varx: %f (i.e. 2555/1296)\n', ux, varx);
+printf('(c) ux: %f (i.e. 301/36), varx: %f (i.e. 2555/1296)\n', ux, varx);
 if (OCTAVE_VERSION)
 	print -dpng 9.png; print -dsvg 9.svg;
 endif
