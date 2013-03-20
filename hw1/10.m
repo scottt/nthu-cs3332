1d99f80c9c1809e526c155d471200e125caaf01d
Scott Tsai <scottt.tw@gmail.com>
2013-03-20 18:59:26 +0800

    Simplify variance calculations

--- hw1/10.m
+++ hw1/10.m
@@ -1,10 +1,8 @@
 % Problem 10, P(X=i) = gamma/(i + 1), i = {0, 1, 2, 3, 4}
-gamma = 60/137; % solved by hand using sum(P) == 1
-Px = @(x) gamma/(x+1);
+g = 60/137; % solved by hand using sum(P) == 1
 X = 0:4;
-P = arrayfun(Px, X);
+P = g ./ (X + 1);
 ux = X * P';
-sx2 = arrayfun(@(x) x^2, X) * P';
-varx = sx2 - ux^2;
-fprintf('(a) gamma: %f (i.e. 60/137)\n(b) ux: %f (i.e. 163/137)\n', gamma, ux);
-fprintf('(c) varx: %f (i.e. 33300/18769)\n', varx);
+sx2 = X.^2 * P' - ux^2;
+fprintf('(a) gamma: %f (i.e. 60/137)\n(b) ux: %f (i.e. 163/137)\n', g, ux);
+fprintf('(c) varx: %f (i.e. 33300/18769)\n', sx2);
--- hw1/9.m
+++ hw1/9.m
@@ -1,19 +1,17 @@
 % Problem 9: throw two dice. r.v. X is min of dice face
-Px = @(x) (13 - 2*x)/36;
-fprintf('(a) p.m.f.: P(X=x) = (13 - 2*x)/36\n');
+fprintf('(a) p.m.f.: P(X=x) = (13 - 2*x) / 36\n');
 X = 1:6;
-P = arrayfun(Px, X);
+P = (13 - 2.*X) / 36;
 assert(sum(P) == 1)
 fprintf('(b) See plot\n');
 bar(X, P, 1);
 title('Prob. 9: Dice throwing probability histogram, min of two');
 xlabel('dice face, min of two, six sided');
 ux = X * P';
-sx2 = arrayfun(@(x) x^2, X) * P';
-varx = sx2 - ux^2;
-varx_ = arrayfun(@(x) (x - ux)^2, X) * P'; % alternative way to calculate varx
-assert(abs(varx - varx_)/varx_ < .001)
-fprintf('(c) ux: %f (i.e. 301/36), varx: %f (i.e. 2555/1296)\n', ux, varx);
+sx2 = (X.^2*P') - ux^2;
+sx2_ = (X - ux).^2 * P'; % alternative way to calculate varx
+assert(abs(sx2 - sx2_)/sx2_ < .001)
+fprintf('(c) ux: %f (i.e. 301/36), varx: %f (i.e. 2555/1296)\n', ux, sx2);
 if (exist('OCTAVE_VERSION', 'builtin'))
 	print -dpng 9.png; print -dsvg 9.svg;
 end
